090876cc1ebdbdd7daf4010a873db5895d19db6c
andreimoise1 <moisefacultate1@gmail.com>
2025-05-05 13:59:06 +0300

    Added Storage
    Added Storage

--- Assets/Scripts/Cutter.cs
+++ Assets/Scripts/Cutter.cs
@@ -12,8 +12,8 @@ public class Cutter : MonoBehaviour
     }
     public CutterType cutterType;
     private bool[,] v;
-    private int[] dx = {0, 0, 1, -1};
-    private int[] dy = {1, -1, 0, 0};
+    private int[] dx = { 0, 0, 1, -1 };
+    private int[] dy = { 1, -1, 0, 0 };
     private bool[,,] rasp1 = new bool[10, 3, 3];
     private List<(int, int)> rez = new List<(int, int)>();
     private Queue<(int, int)> q = new Queue<(int, int)>();
@@ -26,7 +26,7 @@ public class Cutter : MonoBehaviour
     {
         v = new bool[3, 3];
         cnt = 0;
-        taieri = new bool[3, 3] {{false, false, false}, {false, false, false}, {false, false, false}};
+        taieri = new bool[3, 3] { { false, false, false }, { false, false, false }, { false, false, false } };
         endPoint = GameObject.Find("EndPoint").transform;
     }
 
@@ -35,7 +35,7 @@ public class Cutter : MonoBehaviour
         taierinoi = taieri;
         if (row >= 0 && row < 3 && col >= 0 && col < 3)
         {
-            taierinoi[row, col] = !taieri[row, col]; // Inversează valoarea
+            taierinoi[row, col] = !taieri[row, col];
             taieri = taierinoi;
         }
     }
@@ -46,7 +46,7 @@ public class Cutter : MonoBehaviour
     }
     private bool Valid(int x, int y)
     {
-        if(x >= 0 && x < 3 && y >= 0 && y < 3)
+        if (x >= 0 && x < 3 && y >= 0 && y < 3)
         {
             return true;
         }
@@ -55,7 +55,7 @@ public class Cutter : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         UnityEngine.Debug.Log(other.tag);
-        if(other.CompareTag("Pick"))
+        if (other.CompareTag("Pick"))
         {
             UnityEngine.Debug.Log("Pick");
             ConveyorBelt conveyorBelt = Object.FindFirstObjectByType<ConveyorBelt>();
@@ -65,38 +65,38 @@ public class Cutter : MonoBehaviour
             }
             v = new bool[3, 3];
             cnt = 0;
-            c = new bool[3, 3]{{false, false, false}, {false, false, false}, {false, false, false}};
+            c = new bool[3, 3] { { false, false, false }, { false, false, false }, { false, false, false } };
             rez.Clear();
             q.Clear();
             BooleanGridRuntimeRenderer a = other.GetComponent<BooleanGridRuntimeRenderer>();
-            for(int i = 0; i < 3; i ++)
+            for (int i = 0; i < 3; i++)
             {
-                for(int j = 0; j < 3; j ++)
+                for (int j = 0; j < 3; j++)
                 {
                     v[i, j] = a.getCell(i, j);
                     //v[i, j] = true;
                 }
             }
-            for(int i = 0; i < 3; i ++)
+            for (int i = 0; i < 3; i++)
             {
-                for(int j = 0; j < 3; j ++)
+                for (int j = 0; j < 3; j++)
                 {
-                    if(v[i, j] && !c[i, j] && !taieri[i, j])
+                    if (v[i, j] && !c[i, j] && !taieri[i, j])
                     {
                         rez.Add((i, j));
                         c[i, j] = true;
                         q.Enqueue((i, j));
                         bool ok = false;
-                        while(q.Count > 0)
+                        while (q.Count > 0)
                         {
                             var tp = q.Dequeue();
-                            for(int h = 0; h < 4; h ++)
+                            for (int h = 0; h < 4; h++)
                             {
                                 int x = tp.Item1 + dx[h];
                                 int y = tp.Item2 + dy[h];
-                                if(Valid(x, y))
+                                if (Valid(x, y))
                                 {
-                                    if(v[x, y] && !c[x, y] && !taieri[x, y])
+                                    if (v[x, y] && !c[x, y] && !taieri[x, y])
                                     {
                                         ok = true;
                                         c[x, y] = true;
@@ -106,12 +106,12 @@ public class Cutter : MonoBehaviour
                                 }
                             }
                         }
-                        if(!ok && cutterType == CutterType.Trasher)
+                        if (!ok && cutterType == CutterType.Trasher)
                             rez.Clear();
                     }
-                    if(rez.Count > 0)
+                    if (rez.Count > 0)
                     {
-                        if(cutterType == CutterType.Recycler)
+                        if (cutterType == CutterType.Recycler)
                         {
                             foreach (var it in rez)
                             {
@@ -123,29 +123,29 @@ public class Cutter : MonoBehaviour
                     }
                 }
             }
-        
-            if(cutterType == CutterType.Trasher)
+
+            if (cutterType == CutterType.Trasher)
             {
-                foreach(var it in rez)
+                foreach (var it in rez)
                     rasp1[cnt, it.Item1, it.Item2] = true;
-                cnt ++;
+                cnt++;
             }
             Vector3 origin = transform.position;
             other.transform.position = origin + new Vector3(1, 0, 0);
             GameObject g2 = null;
-            if(cnt == 1)
+            if (cnt == 1)
             {
-                for(int i = 0; i < 3; i ++)
-                    for(int j = 0; j < 3; j ++)
+                for (int i = 0; i < 3; i++)
+                    for (int j = 0; j < 3; j++)
                         other.GetComponent<BooleanGridRuntimeRenderer>().setCell(i, j, rasp1[0, i, j]);
             }
-            if(cnt == 2)
+            if (cnt == 2)
             {
                 g2 = Instantiate(squarePrefab1, origin + new Vector3(2, 2, 0), Quaternion.identity);
                 g2.transform.SetParent(null);
-                for(int i = 0; i < 3; i ++)
+                for (int i = 0; i < 3; i++)
                 {
-                    for(int j = 0; j < 3; j ++)
+                    for (int j = 0; j < 3; j++)
                     {
 
                         other.GetComponent<BooleanGridRuntimeRenderer>().setCell(i, j, rasp1[0, i, j]);
@@ -154,7 +154,7 @@ public class Cutter : MonoBehaviour
                 }
             }
             other.GetComponent<BooleanGridRuntimeRenderer>().UpdateGridRendering();
-            if(cnt > 1)
+            if (cnt > 1)
             {
                 Wait();
                 g2.GetComponent<BooleanGridRuntimeRenderer>().UpdateGridRendering();
